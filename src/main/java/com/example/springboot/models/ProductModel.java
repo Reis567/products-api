6bfe898119bc9e474dfd11acd4313ffdca1b3d4c
Reis567 <matheusdosreis9@gmail.com>
2024-01-03 15:43:34 -0300

    create a autogenerated field on product model

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,20 @@
+package com.example.springboot.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB-PRODUCTS")
+public class ProductModel implements Serializable {
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID IdProduct;
+}
