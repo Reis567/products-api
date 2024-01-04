264462d911bcbe9f7bfefbda1e89e3d85b297e7f
Reis567 <matheusdosreis9@gmail.com>
2024-01-04 00:31:17 -0300

    base api route

---
+++ src/main/java/com/example/springboot/controllers/BaseController.java
@@ -0,0 +1,17 @@
+package com.example.springboot.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class BaseController {
+
+    @GetMapping
+    public String welcome() {
+        return "Bem-vindo à API";
+    }
+}
